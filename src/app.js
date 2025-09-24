a822824fcb7cac8a77f86646c1c2f0102efea32d
ketan1435 <shendkarketan1435@gmail.com>
2025-09-24 20:33:58 +0530

    Corse Issue Fix second time

--- src/app.js
+++ src/app.js
@@ -41,7 +41,7 @@ app.use(compression());
 const allowedOrigins = [
   'http://localhost:8080',
   'http://127.0.0.1:8080',
-  'https://smartorbiter.netlify.app/
+  'https://smartorbiter.netlify.app',
 ];
 app.use(
   cors({
